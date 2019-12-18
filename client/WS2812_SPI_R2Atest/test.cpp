f7a690c365ef788086931469f012c80a14dba49b
Orange Hsu <orange21201@gmail.com>
2019-12-19 00:22:52 +0800

    1219 update SPI test

--- client/WS2812_SPI_R2Atest/test.cpp
+++ client/WS2812_SPI_R2Atest/test.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <bcm2835.h>
 #include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
@@ -11,8 +12,9 @@ using namespace std;
 #define COLOR2 0x11EE22
 
 #define STOP_BYTE 0xFF
-#define LED_ID 0x01
+#define LED_ID 0x00
 #define NUM_LED 24
+#define DATA_OFFSET 3
 
 void genColorSeq(char *, const uint16_t &, const uint8_t, const uint8_t, const uint8_t);
 
@@ -20,35 +22,37 @@ int main()
 {
 	if (!bcm2835_init())
 	{
-	     printf("bcm2835_init failed. Are you running as root??\n");
-	     return 1;
+		printf("bcm2835_init failed. Are you running as root??\n");
+		return 1;
 	}
 	if (!bcm2835_spi_begin())
 	{
-	      printf("bcm2835_spi_begin failed. Are you running as root??\n");
-	      return 1;
+		printf("bcm2835_spi_begin failed. Are you running as root??\n");
+		return 1;
 	}
 
 	uint32_t len = 1024;
-	uint16_t dataLen = 4 + 3 * NUM_LED;
-	char test_seq[dataLen];
+	uint16_t dataLen = 5 + 3 * NUM_LED;
+	char test_seq[dataLen + 1];
 
 	// Setup SPI
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
 	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32);    // div by 32 = 7.8125MHz 
+	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128);    // div by 32 = 7.8125MHz 
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 	
+	printf("LEN = %d\n", dataLen);
 	// for (int i = 0; i < TEST_TIMES; ++i)
 	// {
 		cout << "Send color 1..." << endl;
-		genColorSeq(test_seq, dataLen, 0xFF, 0xFF, 0xFF); // color #FFFFFF
-		bcm2835_spi_transfern(test_seq, len);
+		// genColorSeq(test_seq, dataLen, 0xFF, 0xFF, 0xFF); // color #FFFFFF
+		// bcm2835_spi_transfern(h, strlen(h));
+		// bcm2835_spi_transfern(test_seq, dataLen);
 		// sleep(1);
 		cout << "Send color 2..." << endl;
-		genColorSeq(test_seq, dataLen, 0xAA, 0xAA, 0xAA); // color #AAAAAA
-		bcm2835_spi_transfern(test_seq, len);
+		genColorSeq(test_seq, dataLen, 0xAA, 0xCC, 0xEE); // color #AAAAAA
+		bcm2835_spi_transfern(test_seq, dataLen);
 		// sleep(1);
 	// }
 
@@ -66,9 +70,14 @@ void genColorSeq(char *seq, const uint16_t &len, const uint8_t r, const uint8_t 
 	seq[2] = NUM_LED;
 	for (uint16_t i = 0 ; i < NUM_LED; ++i)
 	{
-		seq[3 * i] = r;
-		seq[3 * i + 1] = g;
-		seq[3 * i + 2] = b;
+		printf("%3d ", 3 * i + DATA_OFFSET);
+		seq[3 * i + DATA_OFFSET] = r;
+		seq[3 * i + DATA_OFFSET + 1] = g;
+		seq[3 * i + DATA_OFFSET + 2] = b;
+
+		printf("%.2X %.2X %.2X\n", seq[3 * i + DATA_OFFSET], seq[3 * i + DATA_OFFSET + 1] , seq[3 * i + DATA_OFFSET + 2]);
 	}
+	seq[len - 2] = 0xFF;
 	seq[len - 1] = 0xFF;
+	// printf("%d\n", strlen(seq));
 }
