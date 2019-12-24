cf9b347c0e995f7d7d0026dd0fd0875d58023ca5
Kenchu123 <k88913n@gmail.com>
2019-12-24 15:00:02 +0800

    test for reading led pg

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,4 +1,14 @@
-import PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js'
 import '../css/index.css'
 
-console.log("hello world")
+const app = new PIXI.Application({
+    width: 800,
+    height: 500
+})
+
+document.body.appendChild(app.view)
+
+const LEDTexture = PIXI.Texture.from('../../test/LED/inputLEDPNG/test3.png')
+const LEDSprite = new PIXI.Sprite(LEDTexture)
+LEDSprite.scale.set(40, 40)
+app.stage.addChild(LEDSprite)
