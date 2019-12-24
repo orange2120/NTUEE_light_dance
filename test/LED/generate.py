47cbf1d162b8cb18a74203c2a1efd4fac57f4704
System Ato <cornerhipster@gmail.com>
2019-12-24 09:46:56 +0800

    modified generate.py and readme

--- test/LED/generate.py
+++ test/LED/generate.py
@@ -6,28 +6,38 @@ import os
 
 WS_LEN = [88]
 N_WS = 1
-# check if output directory exists
-if not os.path.exists('outputRGB') :
-    os.makedirs('outputRGB')
-    print('create directory : outputRGB')
+
 
 # convert file to rgb array
-with open('asset.json') as json_file:
-    data = json.load(json_file)
-    for imgPath in data['path']:
-        img = Image.open('inputLEDPNG/' + imgPath).convert('RGB')
-        print("Read file:" + imgPath)
-        w, h = img.size
-        imgArr = np.array(img).reshape(h, w, 3)
-        imgArr[1::2, :] = imgArr[1::2, ::-1] #invert odd row
-        output_path = 'outputRGB/' + imgPath + '.json'
+def png2rgb(dimension=1):
+    with open('asset.json') as json_file:
+        data = json.load(json_file)
+        for imgPath in data['path']:
+            img = Image.open('inputLEDPNG/' + imgPath).convert('RGB')
+            print("Read file:" + imgPath)
+            w, h = img.size
+            imgArr = np.array(img).reshape(h, w, 3)
+            imgArr[1::2, :] = imgArr[1::2, ::-1] #invert odd row
+            output_path = 'outputRGB/' + imgPath + '.json'
+            
+            with open(output_path, 'w') as outfile:
+                if dimension==1:
+                    json.dump(imgArr.flatten().tolist(), outfile) # 1d array
+                elif dimension ==2:
+                    json.dump(np.reshape(imgArr.flatten(), (-1, 3)).tolist(), outfile) # 2d array
+                elif dimension ==3:
+                    json.dump(imgArr.tolist(), outfile) #3d array
         
-        with open(output_path, 'w') as outfile:
-            # json.dump(imgArr.tolist(), outfile) #3d array
-            json.dump(imgArr.flatten().tolist(), outfile) # 1d array
-            # json.dump(np.reshape(imgArr.flatten(), (-1, 3)).tolist(), outfile) # 2d array
-      
-        print("Save Json file:" + output_path)
+            print("Save Json file(" + str(dimension) + "d):" + output_path)
 
 
 
+if __name__ == '__main__':
+    # check if output directory exists
+    if not os.path.exists('outputRGB') :
+        os.makedirs('outputRGB')
+        print('create directory : outputRGB')
+    if len(argv)>1:
+        png2rgb(int(sys.argv[1]))
+    elif:
+        png2rgb()
