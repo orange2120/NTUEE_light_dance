352f066179136dc4813bd69622832fbe88245379
Kenchu123 <k88913n@gmail.com>
2020-01-29 00:00:53 +0800

    ADD Black Asset and par

--- editor/js/simulator/dancer.js
+++ editor/js/simulator/dancer.js
@@ -1,5 +1,5 @@
 import PIXI from 'pixi.js'
-import { Part, LEDPart } from './part.js'
+import { BlackPart, LightPart, LEDPart } from './part.js'
 
 class Dancer {
     constructor(id, timeline, app, loadTexture) {
@@ -12,6 +12,15 @@ class Dancer {
         this.posX     = 0;          // dancer position
         this.posY     = 0;          // dancer position
         this.parts    = {};         // dancer body part
+        
+        // BlackPart
+        this.parts["BLHAT"] = new BlackPart(this, "BLHAT", this.app);
+        this.parts["BLFACE"] = new BlackPart(this, "BLFACE", this.app);
+        this.parts["BLCOAT"] = new BlackPart(this, "BLCOAT", this.app);
+        this.parts["BLHAND"] = new BlackPart(this, "BLHAND", this.app);
+        this.parts["BLINNER"] = new BlackPart(this, "BLINNER", this.app);
+        this.parts["BLPANTS"] = new BlackPart(this, "BLPANTS", this.app);
+        this.parts["BLSHOES"] = new BlackPart(this, "BLSHOES", this.app);
 
         // this.parts["A"]  = new Part();
         // this.parts["B"]  = new Part();
@@ -19,8 +28,8 @@ class Dancer {
         // this.parts["D"]  = new Part();
         // this.parts["E"]  = new Part();
         // this.parts["F"]  = new Part();
-        // this.parts["G"]  = new Part();
-        this.parts["LEDH"] = new LEDPart(this.id, this.app, loadTexture["LEDH"]);    // LED Head
+        // this.parts["HAT"]  = new LightPart(this, this.app, loadTexture["HAT"]);
+        // this.parts["LEDH"] = new LEDPart(this, this.app, loadTexture["LEDH"]);    // LED Head
         // this.parts["LEDH"] = new LEDPart();
         // this.parts["LEDH"] = new LEDPart();
     }
@@ -32,7 +41,11 @@ class Dancer {
 
     checkUpdate(t) {
         const newTime = t;
-        if (newTime >= this.timeline[this.timeInd]["End"]) {
+        if (!this.timeline[this.timeInd + 1]) {
+            // TODO: clear interval
+            return false;
+        }
+        if (newTime >= this.timeline[this.timeInd + 1]["Start"]) {
             this.timeInd += 1;
             this.setStat(this.timeline[this.timeInd]["Status"]);
             return true;
