7e6874af4730df43744400c1d36ec2acd082b0c5
Kenchu123 <k88913n@gmail.com>
2020-01-29 00:00:53 +0800

    ADD Black Asset and par

--- editor/js/constants/simulator.js
+++ editor/js/constants/simulator.js
@@ -1,5 +1,35 @@
-export const DANCER_NUM = 3
-export const DISPLAY_WIDTH = 1200
-export const DISPLAY_HEIGHT = 700
+export const DANCER_NUM = 1
+export const DISPLAY_WIDTH = 1400
+export const DISPLAY_HEIGHT = 800
 export const FPS = 30
+export const PARTARGS = {
+    "BLHAT": {
+        width: 121.563,
+        height: 43.694
+    },
+    "BLFACE": {
+        width: 75.58,
+        height: 97.19,
+    },
+    "BLCOAT": {
+        width: 234.281,
+        height: 188.708
+    },
+    "BLHAND": {
+        width: 247.15,
+        height: 32.513
+    },
+    "BLINNER": {
+        width: 89.571,
+        height: 198.667
+    },
+    "BLPANTS": {
+        width: 137.755,
+        height: 145.208
+    },
+    "BLSHOES": {
+        width: 201.079,
+        height: 35.974
+    }
+}
 
