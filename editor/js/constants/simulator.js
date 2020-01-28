e820ac7f64996d3b21fae05c1e91baa814c19b8d
Kenchu123 <k88913n@gmail.com>
2020-01-29 01:02:49 +0800

    Add Black dancer to position

--- editor/js/constants/simulator.js
+++ editor/js/constants/simulator.js
@@ -5,31 +5,45 @@ export const FPS = 30
 export const PARTARGS = {
     "BLHAT": {
         width: 121.563,
-        height: 43.694
+        height: 43.694,
+        x: 69.065,
+        y: 9.891
     },
     "BLFACE": {
         width: 75.58,
         height: 97.19,
+        x: 91.607,
+        y: 56.212
     },
     "BLCOAT": {
         width: 234.281,
-        height: 188.708
+        height: 188.708,
+        x: 11.629,
+        y: 152.287
     },
     "BLHAND": {
         width: 247.15,
-        height: 32.513
+        height: 32.513,
+        x: 4.89,
+        y: 340.995
     },
     "BLINNER": {
         width: 89.571,
-        height: 198.667
+        height: 198.667,
+        x: 84.387,
+        y: 148.131
     },
     "BLPANTS": {
         width: 137.755,
-        height: 145.208
+        height: 145.208,
+        x: 60.295,
+        y: 330.309
     },
     "BLSHOES": {
         width: 201.079,
-        height: 35.974
+        height: 35.974,
+        x: 30.129,
+        y: 481.508
     }
 }
 
