95d3b7c3c74910e6b51084ef31a65f6845d01e07
System Ato <cornerhipster@gmail.com>
2020-02-11 13:35:41 +0800

    edit platform

--- server/scan.sh
+++ server/scan.sh
@@ -2,8 +2,8 @@ OS=`uname`
 IO="" # store IP
 echo "$OS"
 case $OS in
-   Linux) IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;;
-   FreeBSD|OpenBSD|Darwin) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
+   Linuxs) IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;;
+   FreeBSD|OpenBSD|Darwin|Linux) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
    SunOS) IP=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{ print $2} '` ;;
    *) IP="Unknown";;
 esac
