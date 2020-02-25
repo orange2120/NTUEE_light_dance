b2e240b5e0c64fc04f19582f90ecd7c579d3800e
Kenchu123 <k88913n@gmail.com>
2020-02-25 17:56:02 +0800

    update new svg

--- editor/js/constants/simulator.js
+++ editor/js/constants/simulator.js
@@ -40,9 +40,9 @@ export const DANCERPOS = [
 // All Parts
 export const BLPARTS = ["BLHAT", "BLFACE", "BLCOAT", "BLHAND", "BLINNER", "BLPANTS", "BLSHOES"];
 export const LIGHTPARTS = [
-    "HAT1", "HAT2", "FACE1", "FACE2", "INNER1", "INNER2", "L_COAT1", "L_COAT2", "R_COAT1", "R_COAT2",
+    "HAT1", "HAT2", "FACE1", "INNER1", "INNER2", "L_COAT1", "L_COAT2", "R_COAT1", "R_COAT2",
     "L_ARM1", "L_ARM2", "R_ARM1", "R_ARM2", "L_HAND", "R_HAND", "L_PANTS1", "L_PANTS2", "R_PANTS1",
-    "R_PANTS2", "L_SHOES1", "L_SHOES2", "R_SHOES1", "R_SHOES2"
+    "R_PANTS2", "L_SHOES1", "R_SHOES1"
 ];
 export const LEDPARTS = ["LED_CHEST", "LED_R_SHOE", "LED_L_SHOE"];
 
@@ -117,13 +117,6 @@ export const PARTARGS = {
         height: 119.19,
         x: -82 + 201.374,
         y: -41 + 115.57
-    },
-    "FACE2": {
-        zIndex: 5,
-        width: 67.836,
-        height: 109.536,
-        x: -82 + 212.331,
-        y: -41 + 119.42
     },
     "L_COAT1": {
         zIndex: 10,
@@ -244,26 +237,12 @@ export const PARTARGS = {
         x: -82 + 128.681,
         y: -41 + 637.139
     },
-    "L_SHOES2": {
-        zIndex: 10,
-        width: 69.916,
-        height: 34.176,
-        x: -82 + 133.226,
-        y: -41 + 642.109
-    },
     "R_SHOES1": {
         zIndex: 10,
         width: 78.989,
         height: 44.117,
         x: -82 + 289.201,
         y: -41 + 637.139
-    },
-    "R_SHOES2": {
-        zIndex: 10,
-        width: 69.905,
-        height: 34.176,
-        x: -82 + 293.729,
-        y: -41 + 642.109
     },
     "LED_CHEST": {
         zIndex: 3,
