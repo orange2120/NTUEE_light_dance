384cbeccadb0a7361c628977a98eeb65ec40ca8e
System Ato <cornerhipster@gmail.com>
2020-03-01 18:00:26 +0800

    update

--- client/clientApp/client.js
+++ client/clientApp/client.js
@@ -132,8 +132,8 @@ function main() {
             console.log("Done")
           } else if (msg.type === "play") {
             console.log(`Play from ${msg.data.play_from_time}`)
-            console.log('run ' + String(msg.data.play_from_time))
-            clientApp_cmd.stdin.write('run ' + String(msg.data.play_from_time))
+            // console.log('run ' + String(msg.data.play_from_time))
+            clientApp_cmd.stdin.write('run ' + String(msg.data.play_from_time) + '\n')
             console.log('Done')
           } else if (msg.type === "pause") {
             console.log(`Pause from Server`)
