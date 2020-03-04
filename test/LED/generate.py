f7e067f4606ddb0d7afb0f42b915cd57bc871ccf
System Ato <atosystem@hotmail.com>
2020-03-04 23:57:12 +0800

    alter generate.py function

--- test/LED/generate.py
+++ test/LED/generate.py
@@ -10,29 +10,50 @@ N_WS = 1
 
 # convert file to rgb array
 def png2rgb(dimension=1,rotate=0):
-    with open('asset.json') as json_file:
-        data = json.load(json_file)
-        for imgPath in data['path']:
-            img = Image.open('inputLEDPNG/' + imgPath).convert('RGB')
-            print("Read file:" + imgPath)
+    for filename in os.listdir('inputLEDPNG/'):
+        if filename.endswith(".png"):
+            print("Read file:" + filename)
+
+            img = Image.open('inputLEDPNG/' + filename).convert('RGB')
             w, h = img.size
             imgArr = np.array(img).reshape(h, w, 3)
 
-            
-            # rotate 90*rotate degree
+             # rotate 90*rotate degree
             imgArr = np.rot90(imgArr,rotate)
+
             imgArr[1::2, :] = imgArr[1::2, ::-1] #invert odd row
-            output_path = 'outputRGB/' + imgPath + '.json'
-            
+
+            output_path = 'outputRGB/' + os.path.splitext(filename)[0] + '.json'
+
             with open(output_path, 'w') as outfile:
-                if dimension==1:
-                    json.dump(imgArr.flatten().tolist(), outfile) # 1d array
-                elif dimension ==2:
-                    json.dump(np.reshape(imgArr.flatten(), (-1, 3)).tolist(), outfile) # 2d array
-                elif dimension ==3:
-                    json.dump(imgArr.tolist(), outfile) #3d array
+                json.dump(imgArr.flatten().tolist(), outfile) # 1d array
+            print("Save Json file(1d)(rotate",rotate*90 ,"):" + output_path)
+            
+    return
+
+    # with open('asset.json') as json_file:
+    #     data = json.load(json_file)
+    #     for imgPath in data['path']:
+    #         img = Image.open('inputLEDPNG/' + imgPath).convert('RGB')
+    #         print("Read file:" + imgPath)
+    #         w, h = img.size
+    #         imgArr = np.array(img).reshape(h, w, 3)
+
+            
+    #         # rotate 90*rotate degree
+    #         imgArr = np.rot90(imgArr,rotate)
+    #         imgArr[1::2, :] = imgArr[1::2, ::-1] #invert odd row
+    #         output_path = 'outputRGB/' + imgPath + '.json'
+            
+    #         with open(output_path, 'w') as outfile:
+    #             if dimension==1:
+    #                 json.dump(imgArr.flatten().tolist(), outfile) # 1d array
+    #             elif dimension ==2:
+    #                 json.dump(np.reshape(imgArr.flatten(), (-1, 3)).tolist(), outfile) # 2d array
+    #             elif dimension ==3:
+    #                 json.dump(imgArr.tolist(), outfile) #3d array
         
-            print("Save Json file(" + str(dimension) + "d):" + output_path)
+    #         print("Save Json file(" + str(dimension) + "d):" + output_path)
 
 
 
