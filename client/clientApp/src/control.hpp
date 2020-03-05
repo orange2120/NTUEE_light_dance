d03bde814fab827f012e5f8a125031b9232db1e2
JIMntuee <b07901036@g.ntu.edu.tw>
2020-03-06 00:56:43 +0800

    3/6 0056

--- client/clientApp/src/control.hpp
+++ client/clientApp/src/control.hpp
@@ -86,8 +86,9 @@ void sendSig(const int id) {
     }
     // send LED sig
     for(int i = 0; i < NUM_OF_LED; ++i) {
+        if(e.LED_parts[i]->get_data() == 0) continue;
         leds.sendToStrip(i, e.LED_parts[i]->get_data());
-        if(i != NUM_OF_LED-1)   usleep(25000);
+        if(i != NUM_OF_LED-1)   usleep(LED_DELAY);
     }
 }
 
