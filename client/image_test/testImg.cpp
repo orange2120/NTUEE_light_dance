e94014d3c83e8346f39cae19541b92b2bd2db9a5
JIMntuee <b07901036@g.ntu.edu.tw>
2020-03-09 17:25:04 +0800

    testImg

--- client/image_test/testImg.cpp
+++ client/image_test/testImg.cpp
@@ -16,23 +16,34 @@
 #include "definition.h"
 #include "LED_strip.h"
 
-#define alpha 0.2
-#define TEST_INTERVAL 500000 // us
-
-
 using json = nlohmann::json;
 using namespace std;
 
+#define alpha 1
+#define TEST_INTERVAL 500000 // us
+
+const string Dir = "./energy_json/";
+const string Ending = ".json";
 int id = 0, dataSize = 0;
-char* RGB_data = NULL;
-string path = "./test.json";
 LED_Strip leds(NUM_OF_LED, numLEDs);
+
+
+bool cmpEnding(const string& fullstr, const string& ending) { // compare ending of FileName
+    if(fullstr.length() >= ending.length()) {
+        return ( 0 == fullstr.compare(fullstr.length() - ending.length(), ending.length(), ending);
+    }
+    else return false;
+}
+
+void getFiles(const string&, vector<string>&);
+void readFile(const vector<string>&, vector<char*>&);
+
+
 
 int main(int argc, char *argv[])
 {
-    if (argc == 3)
-    {
-        path = argv[2];
+    if(argc == 3) {
+        Dir = argv[2];
     }
     else if (argc != 2)
 	{
@@ -42,24 +53,55 @@ int main(int argc, char *argv[])
 	}
 
     id = atoi(argv[1]);
-    ifstream infile(path);
-    json RGB = json::parse(infile);
-    dataSize = RGB.size();
-    RGB_data = new char[dataSize];
-    for(int i = 0; i < dataSize; ++i) {
-        double tmp = RGB[i];
-        tmp *= alpha;
-        RGB_data[i] = char(int(tmp));
+    if(id == 0) dataSize = 300;
+    else dataSize = 40;
+    vector<string> FileNames;
+    vector<char*>  RGBs;
+    getFiles(Dir, FileNames);
+    readFile(FileNames, RGBs);
+    for(int i : RGBs.size()) {
+        leds.sendToStrip(id, RGBs[i]);
+        usleep(TEST_INTERVAL);
     }
-    leds.sendToStrip(id, RGB_data);
-    usleep(TEST_INTERVAL);
 
     // turnoff
-    // char* tmp = 0;
-    // tmp = new char[dataSize];
-    // for(int j = 0; j < dataSize; ++j) tmp[j] = 0;
-    // leds.sendToStrip(id, tmp);
-    // delete[] tmp;
-    // usleep(TEST_INTERVAL);
+    char* tmp = 0;
+    tmp = new char[dataSize];
+    for(int j = 0; j < dataSize; ++j) tmp[j] = 0;
+    leds.sendToStrip(id, tmp);
+    delete[] tmp;
+    usleep(TEST_INTERVAL);
     cout << "done!" << endl;
+}
+
+void getFiles(const string& dir_path, vector<string>& files) {
+    DIR* dp;
+    dirent* dirp;
+    if((dp = opendir(dir_path.c_str())) == NULL) {
+        cout << "can't open " << dir_path << "!!" << endl;
+        return;
+    }
+    while ((dirp = readdir(dp)) != NULL ) {
+        // cerr << dirp->d_name << endl;
+        // FIXME: test dir/file
+        if(cmpEnding(dirp->d_name, Ending)) {
+            files.push_back(dirp->d_name);
+        }
+    }
+    closedir(dp);
+}
+
+void readFile(const vector<string>& files, vector<char*>& RGB_datas) {
+    for(int i : files.size()) {
+        ifstream infile(files[i]);
+        json RGB = json::parse(infile);
+        char* data = new char[dataSize];
+        for(int j : dataSize) {
+            double tmp = RGB[i];
+            tmp *= alpha;
+            data[i] = char(int(tmp));
+        }
+        RGB_datas.push_back(data);
+        delete[] data;
+    }
 }
