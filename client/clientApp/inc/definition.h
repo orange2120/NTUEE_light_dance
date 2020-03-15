e64ce8088749cf4b085434fd720ee56c281adcfe
System Ato <atosystem@hotmail.com>
2020-03-16 00:15:44 +0800

    update definition

--- client/clientApp/inc/definition.h
+++ client/clientApp/inc/definition.h
@@ -40,12 +40,16 @@ const std::string FILENAME = "timeline.json";
 
 // EL parts' name
 const std::string ELs[NUM_OF_EL] = {
-  "HAT1", "HAT2", "FACE1", "FACE2", 
-  "R_COAT1", "R_COAT2", "L_COAT1", "L_COAT2",
-  "INNER1", "INNER2", "R_ARM1", "R_ARM2", 
-  "R_HAND", "L_ARM1", "L_ARM2", "L_HAND", 
-  "R_PANTS1", "R_PANTS2", "L_PANTS1", "L_PANTS2", 
-  "R_SHOES1", "L_SHOES1"
+  "HAT1", "HAT2" , "FACE1", "FACE2" , 
+  "L_HAND" , "R_HAND",
+  "L_ARM1", "L_ARM2",
+  "R_ARM1", "R_ARM2",
+  "L_COAT1", "L_COAT2",
+  "R_COAT1", "R_COAT2",
+  "INNER1", "INNER2",
+  "L_PANTS1","L_PANTS2",
+  "R_PANTS1","R_PANTS2",
+  "L_SHOES1","R_SHOES1"
 };
 
 // LED parts' name
