a597d4abb99c23c3b0236e6652fd73eecf36972a
Kenchu123 <k88913n@gmail.com>
2020-03-16 00:49:50 +0800

    trying

--- client/clientApp/inc/definition.h
+++ client/clientApp/inc/definition.h
@@ -40,18 +40,19 @@ const std::string FILENAME = "timeline.json";
 
 // EL parts' name
 const std::string ELs[NUM_OF_EL] = {
-  "R_COAT2", "R_COAT1",
   "L_COAT2", "L_COAT1",
   "R_ARM2", "R_ARM1",
   "L_ARM2", "L_ARM1",
-  "R_HAND", "L_HAND" ,
+  "L_HAND", "R_HAND",
   "INNER1", "INNER2",
-  "FACE1", "FACE2" , "HAT1", "HAT2" , 
+  "R_COAT2", "R_COAT1",
+  "FACE2", "FACE1", "HAT1", "HAT2",
 
-  "R_PANTS1","R_PANTS2",
-  "L_PANTS1","L_PANTS2",
-  "L_SHOES1","R_SHOES1"
+  "L_PANTS2", "L_PANTS1",
+  "R_SHOES1", "L_SHOES1",
+  "R_PANTS2", "R_PANTS1"
 };
+
 
 // LED parts' name
 const std::string LEDs[NUM_OF_LED] = {
