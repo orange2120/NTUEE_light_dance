7fef7f47f0d2ce787382da969ce292e358e1609a
System Ato <atosystem@hotmail.com>
2020-04-07 13:24:00 +0800

    temp make_clientApp

--- client/clientApp/make_clientApp.sh
+++ client/clientApp/make_clientApp.sh
@@ -1,7 +1,5 @@
 cd /home/pi/NTUEE_light_dance/client/clientApp/
-make clean
-make
-cd /home/pi/NTUEE_light_dance/client/turnOffLED/
-make clean
-make
+sudo pm2 delete 0
+sudo pm2 start pm2_startup.sh
+sudo pm2 save
 echo "FINISH"
